e9ae2fd935f9ec225370a868b9a2445aa2eec4a4
conner.kaeden22 <conner.kaeden22@gmail.com>
2023-04-08 18:14:04 +0000

    Saving this progress

---
+++ js-basics-learning/index.js
@@ -0,0 +1,19 @@
+// https://youtu.be/W6NZfCO5SIk?t=1669 video progress
+let name = "person";
+console.log(name);
+// Variables cannot be named reserved keywords.
+// Variable names should be meaningful
+// Variable names cannot start with a number
+// They cannot contain spaces or hyphens 
+// Variable names are case sensitive
+let firstName;
+
+//Constants
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+//Variables change, const stays the same
+
+//Use null when you want to clear the value of a variable
